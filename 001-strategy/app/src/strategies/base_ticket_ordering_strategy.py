26f7e142c23b04658591393e057953b83736f7b6
henk-vd-brink <hcebrink@gmail.com>
2021-12-25 15:53:03 +0100

    refactor

--- 001-strategy/app/src/strategies/base_ticket_ordering_strategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-from ..support_ticket import SupportTicket
-
-class TicketOrderingStrategy(ABC):
-    @abstractmethod
-    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
-        pass
